1504d8d958423dc0b767ac15f70830bcd7d62d61
unknown <annangquoc134@gmail.com>
2022-12-08 14:45:56 -0500

    admin add skill,user, delets skill, delete user working

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,14 +1,57 @@
+import { Tab } from "@headlessui/react";
 import AddSkill from "./admin/AddSkill";
 import DisplaySkills from "./admin/DisplaySkills";
+import classNames from "classnames";
+import AddUser from "./admin/AddUser";
+import DisplayUsers from "./admin/DisplayUsers";
 
-const Admin  = () => {
-    return (
-        <div className="flex border flex-row w-full h- content-center p-4 justify-between items-left"> 
+const Admin = () => {
+  return (
+    <Tab.Group>
+      <Tab.List className="flex justify-center space-x-1 rounded-xl  p-1">
+        <Tab
+          className={({ selected }) =>
+            classNames(
+              "border border-black w-1/3 backdrop-blur-md rounded-lg py-1 text-sm font-bold leading-5 ",
+              "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+              selected
+                ? "bg-blue-50 shadow text-blue-700"
+                : "text-gray-400 hover:bg-white/[0.12] hover:text-blue-700"
+            )
+          }
+        >
+          Skills
+        </Tab>
+        <Tab
+          className={({ selected }) =>
+            classNames(
+              "border border-black w-1/3 rounded-lg py-1 backdrop-blur-md text-sm font-bold leading-5 ",
+              "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+              selected
+                ? "bg-blue-50 shadow text-blue-700"
+                : "text-gray-400 hover:bg-white/[0.12] hover:text-blue-700"
+            )
+          }
+        >
+          Users
+        </Tab>
+      </Tab.List>
+      <Tab.Panels>
+        <Tab.Panel>
+          <div className="flex border flex-row w-full content-center p-4 justify-between items-left">
             <AddSkill />
-            <DisplaySkills/>
-        </div>
-        
-     );
-}
- 
+            <DisplaySkills />
+          </div>
+        </Tab.Panel>
+        <Tab.Panel>
+          <div className="flex border flex-row w-full content-center p-4 justify-between items-left">
+            <AddUser />
+            <DisplayUsers />
+          </div>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  );
+};
+
 export default Admin;
