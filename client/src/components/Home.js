0210c16eeb9b77262806cb3d1e088cfbcf21eeff
unknown <annangquoc134@gmail.com>
2022-12-13 14:27:42 -0500

    basic change to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
-const Home  = () => {
-    return ( 
-        <div>
-            In Home
-        </div>
-     );
-}
- 
+const Home = () => {
+  return (
+    <div className="flex flex-col h-full justify-center place-content-center">
+      <div className="justify-center text-center">Welcome to Connect Lite!</div>
+      <div className="justify-center text-center">Please log in to see your skills!</div>
+    </div>
+  );
+};
+
 export default Home;
